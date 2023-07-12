232fd2ab4fa79e5be9c50919f2be39a46f4b1e87
nooh3 <chosh313@naver.com>
2023-07-12 09:54:21 +0900

    초기 세팅 완료

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div><img link alt="logo" />
+            <img alt="button" />
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
